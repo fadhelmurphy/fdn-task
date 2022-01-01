259c293259a58eb0236adee126993caaa17eb301
Fadhel ijlal falah <fadhelijlalfalah@gmail.com>
2022-01-01 23:59:54 +0700

    [fadhel] fdn-task

--- components/cards/articles.jsx
+++ components/cards/articles.jsx
@@ -3,6 +3,24 @@ import { Card,Row,Col } from "react-bootstrap";
 import { Rating } from 'react-simple-star-rating'
 export const ArticleCards = ({data=null}) => {
   return (
+    <>
+    <style global>
+      {`
+      .article-text-sponsored{
+        position: absolute;
+        top: 0px;
+        left: 8px;
+        margin: 0;
+        background-color: #FFFFFF;
+        color: #DB284E;
+        font-size: 10px;
+        line-height: 10px;
+        letter-spacing: 0px;
+        text-align: left;
+        text-transform: uppercase;
+      }
+      `}
+    </style>
     <Row>
       {data.map(({title,image,author},idx)=>(
         
@@ -11,7 +29,7 @@ export const ArticleCards = ({data=null}) => {
       <Card.Img variant="top" className="img-fluid w-100" 
       style={{maxHeight:'200px',borderRadius:"10px"}} 
       src={image} />
-         
+         {((idx%2)==0)&&<p class="article-text-sponsored fw-bolder px-2 py-1">Sponsored</p>}
           <Card.Text className="py-3">
               <h4 className="fw-bolder">{title}</h4>
               <p><span className="fw-normal">{author}</span>
@@ -22,5 +40,6 @@ export const ArticleCards = ({data=null}) => {
     </Card>
       ))}
     </Row>
+    </>
   );
 };
